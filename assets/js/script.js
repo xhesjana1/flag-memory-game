007bb633f25001e1f4088ef83f21c103ddc7183e
Xhesjana Rustemi <xhesjana1@gmail.com>
2025-03-29 11:10:10 +0000

    Add images and create DOM and a list of array

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Wait for the DOM to finish loading before running the game
+// Add event listeners 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cardsArea = document.getElementById("cards-area");
+
+    // 8 Unique Flags 
+    const flagImages = [
+        "albania.jpg", "newzealand.png", "australia.png", "cambodia.png",
+        "palestine.webp", "ireland.png", "italy.png", "kosovo.png"
+    ];
+}
